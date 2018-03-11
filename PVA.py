62c6d48aefb421c736d69f4c9f2d4a5eb2b03a45
Raunak <Raunakagrawal1001@gmail.com>
2018-03-12 00:24:16 +0530

    Connections Added

--- PVA.py
+++ PVA.py
@@ -4,6 +4,79 @@ def initialiseCounter(data):
 	counter=int(newAtom[1])
 	return counter
 
+def createStartConnectingData(connectionData,startHydrogen,endHydrogen,num):
+	finalConnection=[]
+	for i in range(0,num):
+		connection=connectionData[i].split()
+		if str(connection[1])==str(endHydrogen):
+			continue
+		newConnection=[]
+		for j in connection:
+			if str(j)!=str(endHydrogen):
+				if str(j)=='CONECT':
+					newConnection.append(j)
+				elif int(j)>endHydrogen	:
+					newConnection.append(str(int(j)-1))
+				else:
+					newConnection.append(j)
+		finalConnection.append(newConnection)
+	return finalConnection
+
+
+def createReplicateConnectionData(connectionData,startHydrogen,endHydrogen,num):
+	finalConnection=[]
+	for i in range(0,num):
+		connection=connectionData[i].split()
+		if str(connection[1])==str(endHydrogen) or str(connection[1])==str(startHydrogen):
+			continue
+		newConnection=[]
+		for j in connection:
+			if str(j)!=str(endHydrogen) and str(j)!=str(startHydrogen):
+				if str(j)=='CONECT':
+					newConnection.append(j)
+				else: 
+					if int(j)>startHydrogen:
+						j=int(j)-1
+					if int(j)>=endHydrogen:
+						j=int(j)-1
+					newConnection.append(str(j))
+		finalConnection.append(newConnection)
+	return finalConnection
+
+def creatConeectionDataMiddle(num,replicateConnectingData,i):
+	newConnection=[]
+	for mlist in replicateConnectingData:
+		intermediateConnection=[]
+		for j in mlist:
+			if j=="CONECT":
+				intermediateConnection.append(j)
+			else:
+				# print j
+				intermediateConnection.append(str(int(j)+i))
+		newConnection.append(intermediateConnection)
+	return newConnection
+
+
+def createLastMoleculeConnectingData(connectionData,units,endHydrogen,num):
+	diff=(num-2)*(units-2)+num-1
+	finalConnection=[]
+	for i in range(0,num):
+		connection=connectionData[i].split()
+		if str(connection[1])==str(startHydrogen):
+			continue
+		newConnection=[]
+		for j in connection:
+			if str(j)!=str(startHydrogen):
+				if str(j)=='CONECT':
+					newConnection.append(j)
+				elif int(j)>startHydrogen	:
+					newConnection.append(str(int(j)-1+diff))
+				else:
+					newConnection.append(str(int(j)+diff))
+		finalConnection.append(newConnection)
+	return finalConnection
+
+
 def createStartMolecule(data,num,startHydrogen,endHydrogen):
 	startMolecule=[]
 	count=1
@@ -20,28 +93,50 @@ def createStartMolecule(data,num,startHydrogen,endHydrogen):
 	return startMolecule
 
 
+def connectionStringBuilder(mlsit):
+	connection=""
+	for j in mlist:
+		connection=connection+str(j)
+		connection=connection+"    "
+	return connection
+
+
 def buildString(newAtom):
 	if float(newAtom[4]) <10.0:
 		newAtom[4]=format(newAtom[4],'0.3f')
-	elif float(newAtom[4]) >=10.0:
+	elif float(newAtom[4]) <100.0:
 		newAtom[4]=format(newAtom[4],'0.2f')
+	elif float(newAtom[4]) >=100.0:
+		newAtom[4]=format(newAtom[4],'0.1f')
 
 	if float(newAtom[5]) <10.0:
 		newAtom[5]=format(newAtom[5],'0.3f')
-	elif float(newAtom[5]) >=10.0:
+	elif float(newAtom[5]) <100.0:
 		newAtom[5]=format(newAtom[5],'0.2f')
+	elif float(newAtom[5]) >=100.0:
+		newAtom[5]=format(newAtom[5],'0.1f')
+
+
 
 	if float(newAtom[6]) <10.0:
 		newAtom[6]=format(newAtom[6],'0.3f')
-	elif float(newAtom[6]) >=10.0:
+	elif float(newAtom[6]) <100.0:
 		newAtom[6]=format(newAtom[6],'0.2f')
+	elif float(newAtom[6]) >=100.0:
+		newAtom[6]=format(newAtom[6],'0.1f')
+
 
 	flag=float(newAtom[1])
 	spaces=""
 	if flag<10:
 		spaces="    "
-	elif flag>=10:
+	elif flag<100:
 		spaces="   "
+	elif flag<1000:
+		spaces="  "
+	else:
+		spaces=" "
+	
 	spaceX="      "
 	if float(newAtom[4])>=0:
 		spaceX="       "
@@ -59,7 +154,7 @@ def buildString(newAtom):
 def createMainAtom(data,counter,num,connectingAtom):
 	length=len(data)
 	newAtom=list(data[length-num+connectingAtom].split())
-	print newAtom[4]
+	# print newAtom[4]
 	newAtom[1]=counter
 	sourceAtom=data[0].split()
 	mainAtom=data[1].split()
@@ -111,14 +206,19 @@ def  createLastMolecule(data,num,counter,originalMolecule,startHydrogen):
 
 
 f= open("./PVA.pdb","r")
-originalData=f.readlines()
-counter=initialiseCounter(originalData)
-print counter
-units=input("Enter the numbher of repeating units: ")
+fileData=f.readlines()
 startHydrogen=6
 endHydrogen=12
 num=15
 connectingAtom=4
+
+originalData=fileData[1:num+1]
+connectionData=fileData[num+1:]
+sartMoleculeConnectingData=createStartConnectingData(connectionData,startHydrogen,endHydrogen,num)
+replicateConnectingData=createReplicateConnectionData(connectionData,startHydrogen,endHydrogen,num)
+# print replicateConnectingData
+counter=initialiseCounter(originalData)
+units=input("Enter the numbher of repeating units: ")
 startMolecule=createStartMolecule(originalData,num,startHydrogen,endHydrogen)
 data=list(originalData)
 del data[startHydrogen-1]
@@ -131,15 +231,36 @@ if startHydrogen<connectingAtom:
 
 del data[endHydrogen-1]
 
-if endHydrogen<connectingAtom:
+if endHydrogen<=connectingAtom:
 	connectingAtom=connectingAtom-1
+	if units>1:
+		sartMoleculeConnectingData[connectingAtom-1].append(str(num))
+finalConnectionData=sartMoleculeConnectingData
 
 for i in range(1,units-1):
+	newConnectionData=[]
+	if i==1:
+		newConnectionData=(creatConeectionDataMiddle(num-1,replicateConnectingData,num-1))
+	else:
+		newConnectionData=(creatConeectionDataMiddle(num-2,replicateConnectingData,(num-2)*i+1))
 	counter=createMainAtom(data,counter,num-1,connectingAtom)	
 	counter=replicate(data,num-2,counter)
+	if i==1:
+		j=num-1
+	else:
+		j=num-2
+	newConnectionData[0].append(counter-1+connectingAtom-j)
+	newConnectionData[connectingAtom-1].append(counter)
+	# print newConnectionData
+	finalConnectionData=finalConnectionData + (newConnectionData)
 
 counter=createMainAtom(data,counter,num-1,connectingAtom)	
+newConnection=createLastMoleculeConnectingData(connectionData,units,endHydrogen,num)
+newConnection[0].append(counter-1+connectingAtom-num+2)
 createLastMolecule(data,num-1,counter,originalData,startHydrogen)
+finalConnectionData = finalConnectionData + (newConnection)
+# print(finalConnectionData)
+
 f.close()
 f=open("PVA_new.pdb",'w')
 finalMolecule=startMolecule+data[num-2:]
@@ -148,4 +269,11 @@ for atom in finalMolecule:
 	atom=str(atom)
 	atom=atom.strip('\n')
 	f.write("%s\n" % atom)
+for mlist in finalConnectionData:
+	for j in mlist:
+		connection=connectionStringBuilder(j)
+		connection=connection.strip('\n')
+	f.write("%s\n"%connection)
+end="END"
+f.write("%s\n"%end)
 f.close()
